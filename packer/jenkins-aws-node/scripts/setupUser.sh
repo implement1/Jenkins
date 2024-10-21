dc712f269b3326e1d688ef2e06e52badb640383e
implement1 <50425206+implement1@users.noreply.github.com>
2024-10-21 00:27:06 -0400

    packer templates for jenkins and windows  nodes [ci skip]

--- packer/jenkins-aws-node/scripts/setupUser.sh
+++ packer/jenkins-aws-node/scripts/setupUser.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo useradd -d /var/lib/jenkins -m -s /bin/bash jenkins
 sudo mkdir /var/lib/jenkins/.ssh
-sudo echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD6Icn2zLnQGD3R0aKYZzANc2tYjTQOivWVoKfbbRep88SEz2RMpI7ni9Qyq8CFYRRVEczMbcOB3WDlb6s+lfzzWEWU6wPja9yqYl42wlzr0u10McFgmFdiOE6I3onHvl+FQrtu4Qqd+0y+DjGAuh+zHK8KrdJ+wJvAuoBPIHHvUGiuuUS/JmTDBisnAf7aZfX0L7wcAn4a2q/uR+XLBCi/RtPgbniDLorNKDdMnodYquMn6Sq4RT6NprrxVLgyGokmVcfRnvOYB9UX5CNo3dDDKGItyKiKifncXJSIPY4fNjjf9WaOwGWPl4S+l/ojEETfMmJRzUzhxP0aEn/n9OML jenkins@jenkins.nsone.co" | sudo tee /var/lib/jenkins/.ssh/authorized_keys > /dev/null
+sudo echo "ssh-rsa aass jenkins@jenkins.com" | sudo tee /var/lib/jenkins/.ssh/authorized_keys > /dev/null
 sudo chmod 700 /var/lib/jenkins/.ssh
 sudo chmod 644 /var/lib/jenkins/.ssh/authorized_keys
 sudo chown -R jenkins:jenkins /var/lib/jenkins
