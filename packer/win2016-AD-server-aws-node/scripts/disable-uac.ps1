ba7ca1497ce58528aecddd5f2a27e3c8a54dde0e
implement1 <50425206+implement1@users.noreply.github.com>
2024-10-21 00:32:51 -0400

    packer templates for jenkins and windows  nodes [ci skip]

--- packer/win2016-AD-server-aws-node/scripts/disable-uac.ps1
+++
@@ -1,3 +0,0 @@
-Write-Host "Disabling UAC..."
-New-ItemProperty -Path HKLM:Software\Microsoft\Windows\CurrentVersion\Policies\System -Name EnableLUA -PropertyType DWord -Value 0 -Force
-New-ItemProperty -Path HKLM:Software\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin -PropertyType DWord -Value 0 -Force
--- packer/win2016-AD-server-aws-node/scripts/setup_addc.ps1
+++
@@ -1,8 +0,0 @@
-#Requires -Version 3
-#Requires -RunAsAdministrator
-
-Write-Output 'Add Windows features'
-Add-WindowsFeature 'RSAT-AD-Tools'
-Add-WindowsFeature -Name 'ad-domain-services' -IncludeAllSubFeature -IncludeManagementTools
-Add-WindowsFeature -Name 'dns' -IncludeAllSubFeature -IncludeManagementTools
-Add-WindowsFeature -Name 'gpmc' -IncludeAllSubFeature -IncludeManagementTools
